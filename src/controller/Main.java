8c5d1d27aa27dc0dc4bdf3ad3cba7af82a79e4be
3deep0019 <trideepghosh83@gmail.com>
2020-09-25 19:39:36 +0530

    FSJ0820B106

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,30 @@
+package controller;
 
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import dao.SkillDAO;
+
+import model.Skill;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		SkillDAO skilldao=new SkillDAO();
+		
+		 TreeMap<Integer, Skill> map = new TreeMap<Integer, Skill>();
+				
+		 map=skilldao.skillCount();
+		 
+			//	System.out.println("test");
+				
+			for(Entry<Integer, Skill> out: map.entrySet())
+			{
+				int id=out.getKey();
+				Skill skill=out.getValue();
+				System.out.println(skill.getSkillid()+"   "+skill.getSkillName());
+			}
+			}
+	}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,51 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.TreeMap;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+		
+		 public TreeMap<Integer, Skill> skillCount() throws Exception {
+			 TreeMap<Integer,Skill>map = new TreeMap<Integer,Skill>();
+			 
+		 Connection con =ConnectionManager.getConnection();
+		 Statement st=con.createStatement();
+		 String sql="select * from player";
+		 
+		 ResultSet rs=st.executeQuery(sql);
+		 
+		 int batting=0;
+		 int bowling=0;
+		 int wicketkeeping=0;
+		 while(rs.next())
+		 {
+			 if(rs.getLong(4)==1) {
+				 System.out.println("test1"+ rs.getLong(4));
+				 batting++;
+			 }
+			 else if(rs.getLong(4)==2) {
+				 bowling++;
+				 System.out.println("test2"+ rs.getLong(4));
+			 }
+			 else if(rs.getLong(4)==3) {
+				 wicketkeeping++;
+				 System.out.println("test3"+ rs.getLong(4));
+			 }
+		 }
+				 Skill s1=new Skill(1,"batting");
+				 Skill s2=new Skill(2,"bowler");
+				 Skill s3=new Skill(3,"keeper");
+				 
+				 map.put(batting,s1);
+				 map.put(bowling,s2);
+				 map.put(wicketkeeping,s3);
+		return map;
+
+		 
+}
+}
